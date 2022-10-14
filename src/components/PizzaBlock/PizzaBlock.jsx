e4e2946a7ff5298ee872ec03ba14ec03e805049f
VACson <memedealer1337@gmail.com>
2022-10-14 15:47:43 +0300

    add modal window

--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
 
 function PizzaBlock({ image, name, price, sizes, type }) {
+  const [modalIsOpen, setModalIsOpen] = React.useState(false);
+  const [pizzaCount, setPizzaCount] = React.useState(1);
+  const modal = () => {
+    setModalIsOpen(!modalIsOpen);
+  };
   return (
-    <div>
-      <img src={image} alt="" />
-      {name}
-      {price}
+    <div className="pizzablock">
+      <img src={image} className="pizzablock__image" alt="" />
+      <div className="pizzablock__name">{name}</div>
+
+      <div className="pizzablock__buy">
+        <button className="pizzablock__buy__button" onClick={() => modal()}>
+          Додати до кошика
+        </button>
+        <span className="pizzablock__buy__price">{price}₴</span>
+      </div>
+      {modalIsOpen && (
+        <div className="modal">
+          <div className="modal__background" onClick={() => modal()}></div>
+          <div className="modal__popup">
+            <img src={image} className="modal__image" alt="" />
+            <div className="modal__name">{name}</div>
+            {sizes && (
+              <div className="modal__sizeblock">
+                {sizes.map((item) => (
+                  <div className="modal__size" key={item}>
+                    {item}
+                  </div>
+                ))}
+              </div>
+            )}
+            <div className="count">
+              <button className="count__button">-</button>
+              <div className="">{pizzaCount}</div>
+              <button className="count__button">+</button>
+            </div>
+            <div className="modal__order">
+              <button className="modal__button" onClick={() => modal()}>
+                Додати до кошика
+              </button>
+              <span className="modal__price">{price}₴</span>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,13 +3,12 @@ import useAxios from 'axios-hooks';
 import PizzaBlock from '../../components/PizzaBlock/PizzaBlock';
 
 function Home() {
-  const [{ data, loading, error }, refetch] = useAxios('http://localhost:7000/pizzas');
+  const [{ data, loading, error }] = useAxios('http://localhost:7000/pizzas');
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{console.log(error)}Error!</p>;
   return (
-    <div>
-      {console.log(data)}
-      {data.map((pizza, index) => (
+    <div className="pizzalist">
+      {data.map((pizza) => (
         <PizzaBlock
           key={pizza.id}
           image={pizza.imageUrl}
